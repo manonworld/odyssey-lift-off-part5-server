19c4cfccf6381e70e0c7a4f908a04b6586e8f066
mosta <info@manonworld.de>
2021-11-19 16:04:33 +0200

    changed some fields in the schema

--- src/schema.js
+++ src/schema.js
@@ -36,11 +36,13 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The number of modules this track contains"
     modulesCount: Int
     "The track's complete description, can be in markdown format"
     description: String
+    "The track's full duration, in seconds"
+    durationInSeconds: Int
     "The number of times a track has been viewed"
     numberOfViews: Int
     "The track's complete array of Modules"
@@ -62,7 +64,9 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in minutes"
-    length: Int
+    length: Int @deprecated(reason: "Use durationInSeconds")
+    "The module's video duration, in seconds"
+    durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
     content: String
     "The module's video url, for video-based modules"
